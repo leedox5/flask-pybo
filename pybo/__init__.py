6704d861a28da518676089be5cc70566932941db
이명호 <leedox@naver.com>
2021-10-29 16:18:56 +0900

    404 오류처리

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -13,6 +13,10 @@ naming_convention = {
 
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
+
+def page_not_found(e):
+    return render_template("404.html")
 
 
 def create_app():
@@ -36,5 +40,8 @@ def create_app():
 
     from .filter import format_datetime
     app.jinja_env.filters["datetime"] = format_datetime
+
+    # 오류페이지
+    app.register_error_handler(404, page_not_found)
 
     return app
